46bd49cd43f0495e9af63d9641732baa0170f182
Varun Sundar <vsundar4@wisc.edu>
2021-10-13 11:56:52 -0500

    ADD: lcd scene comparison

--- quanta_SL/lcd/scenes/decode_correspondences.py
+++ quanta_SL/lcd/scenes/decode_correspondences.py
@@ -9,6 +9,7 @@ from hydra.utils import get_original_cwd
 from loguru import logger
 from matplotlib import pyplot as plt
 from omegaconf import OmegaConf
+from roipoly import RoiPoly
 
 from quanta_SL.decode.methods import hybrid_decoding, repetition_decoding
 from quanta_SL.decode.minimum_distance.factory import (
@@ -30,9 +31,7 @@ from quanta_SL.utils.plotting import save_plot, ax_imshow_with_colorbar
 from quanta_SL.lcd.decode_helper import decode_2d_code
 
 logger.disable("quanta_SL")
-logger.add(
-    f"logs/lcd_scenes_{Path(__file__).stem}.log", rotation="daily", retention=3
-)
+logger.add(f"logs/lcd_scenes_{Path(__file__).stem}.log", rotation="daily", retention=3)
 
 plt.style.use(["science", "grid"])
 params = {
@@ -165,7 +164,8 @@ def get_hybrid_binary_gt_sequence(cfg, hybrid_code_LUT, rotate_180: bool = False
         binary_frame = load_swiss_spad_bin(
             folder,
             bin_suffix=cfg.hybrid.binary_frame.bin_suffix_range.start
-            + bin_suffix_offset,
+            + bin_suffix_offset
+            + 1,
             num_rows=256,
             num_cols=512,
         )[cfg.hybrid.binary_frame.index]
@@ -205,6 +205,7 @@ def get_hybrid_binary_gt_sequence(cfg, hybrid_code_LUT, rotate_180: bool = False
     binary_sequence = np.stack(binary_sequence, axis=0)
 
     if rotate_180:
+        img = img[::-1, ::-1]
         gt_sequence = gt_sequence[:, ::-1, ::-1]
         binary_sequence = binary_sequence[:, ::-1, ::-1]
 
@@ -237,12 +238,29 @@ def get_repetition_binary_gt_sequence(
         logger.info(f"Sequence #{pattern_index}/{len(pattern_range)}")
 
         # Single binary sample
+
         binary_frame = load_swiss_spad_bin(
             folder,
-            bin_suffix=cfg.hybrid.repetition.bin_suffix_range.start + bin_suffix_offset,
+            bin_suffix=cfg.repetition.binary_frame.bin_suffix_range.start
+            + bin_suffix_offset
+            + 1,
             num_rows=256,
             num_cols=512,
         )
+
+        # Pick multiple samples
+        if cfg.repetition.multi_sample:
+            binary_frame = binary_frame[
+                cfg.repetition.binary_frame.index : cfg.repetition.binary_frame.index
+                + cfg.repetition.repetition_tuple.repeat
+            ]
+        else:
+            binary_frame = binary_frame[
+                cfg.repetition.binary_frame.index : cfg.repetition.binary_frame.index
+                + 1
+            ]
+
+        binary_sequence.append(binary_frame)
 
         # Filename with leading zeros
         bin_suffix_range = (
@@ -258,20 +276,6 @@ def get_repetition_binary_gt_sequence(
         # Groundtruth Frame
         gt_frame = get_gt_frame(folder, bin_suffix_range, comp_bin_suffix_range)
         gt_sequence.append(gt_frame)
-
-        # Pick multiple samples
-        if cfg.repetition.multi_sample:
-            binary_frame = binary_frame[
-                cfg.repetition.binary_frame.index : cfg.repetition.binary_frame.index
-                + cfg.repetition.repetition_tuple.repeat
-            ]
-        else:
-            binary_frame = binary_frame[
-                cfg.repetition.binary_frame.index : cfg.repetition.binary_frame.index
-                + 1
-            ]
-
-        binary_sequence.append(binary_frame)
 
         if cfg.repetition.visualize_frame:
             plt.imshow(gt_frame, cmap="gray")
@@ -292,6 +296,7 @@ def get_repetition_binary_gt_sequence(
     binary_sequence = np.concatenate(binary_sequence, axis=0)
 
     if rotate_180:
+        img = img[::-1, ::-1]
         gt_sequence = gt_sequence[:, ::-1, ::-1]
         binary_sequence = binary_sequence[:, ::-1, ::-1]
 
@@ -329,13 +334,26 @@ def main(cfg):
     )
 
     # Regions to ignore
-    # Ignore shadows and black areas, white pixels
-    mean_hybrid_binary = hybrid_binary_sequence.mean(axis=0)
-    mask = (mean_hybrid_binary < 0.2) | (mean_hybrid_binary > 0.90)
-    plt.figure(figsize=(8, 6))
-    plt.imshow(mean_hybrid_binary)
-    plt.colorbar()
-    plt.show()
+    # Custom RoI
+    mask_path = Path(cfg.outfolder) / "roi_mask.npy"
+    if mask_path.exists():
+        mask = np.load(mask_path)
+
+    else:
+        plt.imshow(img)
+        my_roi = RoiPoly(color="r")
+        my_roi.display_roi()
+
+        mask = my_roi.get_mask(img)
+
+        # Ignore shadows and black areas, white pixels
+        # Valid regions are 1
+        mask_shadows = (img > 0.2) | (img < 0.90)
+        mask = mask & mask_shadows
+
+        np.save(mask_path, mask)
+
+    breakpoint()
 
     # Decode
     logger.info("Decoding Hybrid GT")
@@ -361,12 +379,12 @@ def main(cfg):
     logger.info("Evaluating Accuracy")
 
     hybrid_rmse = np.sqrt(
-        ((hybrid_gt_decoded[~mask] - hybrid_binary_decoded[~mask]) ** 2).mean()
+        ((hybrid_gt_decoded[mask] - hybrid_binary_decoded[mask]) ** 2).mean()
     )
     logger.info(f"RMSE Hybrid {hybrid_rmse}")
 
     repetition_rmse = np.sqrt(
-        ((repetition_gt_decoded[~mask] - repetition_binary_decoded[~mask]) ** 2).mean()
+        ((repetition_gt_decoded[mask] - repetition_binary_decoded[mask]) ** 2).mean()
     )
     logger.info(f"RMSE Repetition {repetition_rmse}")
 
@@ -392,19 +410,19 @@ def main(cfg):
 
     # Row 2: Hybrid ABS, Repeat ABS, GT discrepancy
     hybrid_abs_error = np.abs(hybrid_gt_decoded - hybrid_binary_decoded)
-    hybrid_abs_error[mask] = 0
+    hybrid_abs_error[~mask] = 0
     ax = ax_ll[1, 0]
     ax_imshow_with_colorbar(hybrid_abs_error, ax, fig)
     ax.set_title(f"Hybrid Absolute error | RMSE {hybrid_rmse:.2f}")
 
     repetition_abs_error = np.abs(repetition_gt_decoded - repetition_binary_decoded)
-    repetition_abs_error[mask] = 0
+    repetition_abs_error[~mask] = 0
     ax = ax_ll[1, 1]
     ax_imshow_with_colorbar(repetition_abs_error, ax, fig)
     ax.set_title(f"Repetition Absolute error | RMSE {repetition_rmse:.2f}")
 
     gt_discrepancy = np.abs(hybrid_gt_decoded - repetition_gt_decoded)
-    gt_discrepancy[mask] = 0
+    gt_discrepancy[~mask] = 0
     ax = ax_ll[1, 2]
     ax_imshow_with_colorbar(gt_discrepancy, ax, fig)
     ax.set_title("GT Discrepancy (Hybrid, Repetition)")
